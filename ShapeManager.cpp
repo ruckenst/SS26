920a16075d06ceb0d81d46b7364f4771c1632768
ruckenst <ruckenst@technikum-wien.at>
2026-02-26 15:50:57 +0100

    Präsenz 4

--- ShapeManager.cpp
+++ ShapeManager.cpp
@@ -1,4 +1,5 @@
 #include "ShapeManager.h"
+#include <iostream>
 
 ShapeManager::ShapeManager(int size) {
     maxSize = size;
@@ -9,6 +10,28 @@ ShapeManager::ShapeManager(int size) {
 
 ShapeManager::~ShapeManager() {
     delete[] shapes;
+}
+
+ShapeManager::ShapeManager(const ShapeManager& other) {
+    maxSize = other.maxSize;
+    currentCount = other.currentCount;
+
+    shapes = new Shape[maxSize];
+    for(int i = 0; i < other.currentCount; i++) {
+        shapes[i] = other.shapes[i];
+    }
+}
+
+ShapeManager::ShapeManager(ShapeManager&& other) {
+    std::cout << "MOVE CONSTRUCTOR!" << std::endl;
+
+    maxSize = other.maxSize;
+    currentCount = other.currentCount;
+    shapes = other.shapes;
+
+    other.maxSize = 0;
+    other.currentCount = 0;
+    other.shapes = nullptr;
 }
 
 void ShapeManager::addShape(Shape shape) {
@@ -25,4 +48,44 @@ void ShapeManager::printShapes() const {
     for(int i = 0; i < currentCount; i++) {
         shapes[i].print();
     }
+}
+
+ShapeManager &ShapeManager::operator=(ShapeManager &&other) {
+    std::cout << "MOVE ASSIGMENT OPERATOR" << std::endl;
+
+    if(this == &other) {
+        std::cerr << "Tried to move itself!" << std::endl;
+        return *this;
+    }
+
+    delete[] shapes;
+
+    maxSize = other.maxSize;
+    currentCount = other.currentCount;
+    shapes = other.shapes;
+
+    other.maxSize = 0;
+    other.currentCount = 0;
+    other.shapes = nullptr;
+
+    return *this;
+}
+
+ShapeManager &ShapeManager::operator=(const ShapeManager &other) {
+    if(this == &other) {
+        std::cerr << "Tried to copy itself!" << std::endl;
+        return *this;
+    }
+
+    delete[] shapes;
+
+    maxSize = other.maxSize;
+    currentCount = other.currentCount;
+
+    shapes = new Shape[maxSize];
+    for(int i = 0; i < other.currentCount; i++) {
+        shapes[i] = other.shapes[i];
+    }
+
+    return *this;
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "Shape.h"
 #include "ShapeManager.h"
+#include <iostream>
+
+ShapeManager someFunction(int size) {
+    ShapeManager myTempShapeManager(size);
+    myTempShapeManager.addShape(Shape(1, 1));
+
+    return myTempShapeManager;
+}
 
 int main() {
     ShapeManager shapeManager(10);
@@ -8,7 +16,33 @@ int main() {
     shapeManager.addShape(Shape(43, 12));
     shapeManager.addShape(Shape(3, 4));
 
+    std::cout << std::endl;
     shapeManager.printShapes();
+
+    ShapeManager copiedShapeManager(shapeManager);
+    //ShapeManager copiedShapeManager = shapeManager;
+
+    std::cout << std::endl;
+    copiedShapeManager.printShapes();
+
+    shapeManager.addShape(Shape(84, 23));
+
+    std::cout << std::endl;
+    shapeManager.printShapes();
+
+    std::cout << std::endl;
+    copiedShapeManager.printShapes();
+
+    //ShapeManager movedShapeManger = std::move(copiedShapeManager); // MOVE CONSTRUCTOR
+    copiedShapeManager = std::move(copiedShapeManager); // MOVE ASSIGNMENT OPERATOR
+
+    /*
+    std::cout << std::endl;
+    movedShapeManger.printShapes();
+    */
+
+    std::cout << "HERE COMES COPIED: " << std::endl;
+    copiedShapeManager.printShapes();
 
     return 0;
 }
