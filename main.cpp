12d11f18e839529784e45b94100845090ac9f0c0
ruckenst <ruckenst@technikum-wien.at>
2026-04-28 17:37:10 +0200

    Präsenz 19b

--- main.cpp
+++ main.cpp
@@ -1,72 +1,53 @@
 #include <iostream>
-#include <mutex>
 #include <string>
-#include <thread>
-#include <vector>
 
-void printSomething(bool comparisonFunction(int, int)) {
-    if(comparisonFunction(20, 83)) {
-        std::cout << "Text A" << std::endl;
-    } else {
-        std::cout << "Text B" << std::endl;
-    }
-}
-
-bool testFunction(int a, int b) {
-    return a > b;
-}
-
-struct File {
+struct User {
+    int id;
     std::string name;
-    int size;
 };
 
-void downloadFile(File file, std::mutex& mutex) {
-    int currentSize = 0;
+User* getUserById(int userId){
+    // Load from DB/API/...
+    //return nullptr;
+    return new User(1, "Seppl");
+}
 
-    while(currentSize < file.size) {
-        if(currentSize % 10000 == 0) {
-            double currentPercentage = currentSize / (double)file.size * 100;
+void saveUser(User* user) {
+    // Save User in DB/API/...
+    throw std::out_of_range("DB Access not possible");
+}
 
-            std::lock_guard<std::mutex> guard(mutex);
-            std::cout << "File '" << file.name << "' (" << currentPercentage << "/100)" << std::endl;
-        }
-
-        currentSize++;
+bool updateUsername(int userId, std::string newName) {
+    User* user = getUserById(userId);
+    if(user == nullptr) {
+        throw std::invalid_argument("User not found");
     }
 
-    std::lock_guard<std::mutex> guard(mutex);
-    std::cout << "### File '" << file.name << "' Finished!" << std::endl;
+    user->name = newName;
+
+    saveUser(user);
+
+    // SOMETHING ELSE
+    // SOMETHING ELSE
+    // SOMETHING ELSE
+    // SOMETHING ELSE
+    // SOMETHING ELSE
+    // SOMETHING ELSE
 }
 
 int main() {
-    printSomething(testFunction);
-    printSomething([](int a, int b) {
-        return a < b;
-    });
-
-    std::vector<File> files = {
-            File("File_A.pdf", 892374),
-            File("File_B.pdf", 5231),
-            File("File_C.pdf", 962),
-            File("File_D.pdf", 5682352),
-            File("File_E.pdf", 28235),
-            File("File_F.pdf", 28346),
-            File("File_G.pdf", 6827)
-    };
-
-    std::vector<std::thread> threads;
-    std::mutex mutex;
-
-    for(const auto& file : files) {
-        threads.push_back(std::thread(downloadFile, file, std::ref(mutex)));
+    try {
+        updateUsername(83, "Anna");
+    } catch(const std::out_of_range& ex) {
+        std::cout << "Save not successful! Reason: " << ex.what() << std::endl;
+        return -3;
+    } catch(const std::invalid_argument& ex) {
+        std::cout << "Update not successful! Reason: " << ex.what() << std::endl;
+        return -2;
+    } catch(...) {
+        std::cout << "Unknown error occurred!" << std::endl;
+        return -1;
     }
-
-    for(auto& thread : threads) {
-        thread.join();
-    }
-
-    std::cout << "All files downloaded successfully!" << std::endl;
 
     return 0;
 }
